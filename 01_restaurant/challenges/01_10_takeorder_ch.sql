1e91f459c9945fd1c2b9f13a8566d41aa1df8ae8
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-14 20:25:55 +0000

    01-10 Completed

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,23 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+INSERT INTO Orders (CustomerID, OrderDate)
+SELECT CustomerID, '2022-09-20 14:00:00' AS OrderDate
+FROM Customers
+WHERE FirstName = 'Loretta'
+AND LastName = 'Hundey'
+AND Address = '6939 Elka Place';
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+SELECT 1001 AS OrderID, DishID
+FROM Dishes 
+WHERE Name = 'House Salad'
+OR Name = 'Mini Cheeseburgers'
+OR Name = 'Tropical Blue Smoothie';
+
+SELECT SUM(d.price)
+FROM OrdersDishes AS o
+INNER JOIN Dishes AS d
+ON o.DishID = d.DishID
+WHERE o.OrderID = 1001;
