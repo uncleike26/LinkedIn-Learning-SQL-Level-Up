e2f795fe4b9fd71028cfdabc1aa0dcfed2f0678a
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-14 16:44:11 +0000

    01-08 Completed

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,11 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT r.ReservationID, c.FirstName, c.LastName, r.Date, r.PartySize 
+FROM Customers AS c
+INNER JOIN Reservations AS r
+ON c.CustomerID = r.CustomerID
+WHERE LastName LIKE 'St%n%'
+AND Date LIKE '%-06-14%'
+AND PartySize = 4;
