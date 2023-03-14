f615e281249540a95ede7383f1ddc2a06ad68fd0
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-14 16:10:06 +0000

    01-06 Completed

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,12 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT c.FirstName, c.LastName, r.ReservationID, r.Date
+FROM Customers AS c
+INNER JOIN Reservations AS r
+ON c.CustomerID = r.CustomerID
+WHERE c.FirstName = 'Norby';
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000; 
