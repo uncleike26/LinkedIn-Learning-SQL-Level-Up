fbf163b8e1ae2bc6455bf0c91aeb9bf6e9ef1aa6
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-14 17:00:35 +0000

    01-09 Completed

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,19 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+INSERT INTO Customers(FirstName, LastName, Email)
+SELECT * FROM (SELECT 'Sam', 'McAdams', 'smac@kinetecoinc.com') AS tmp
+WHERE NOT EXISTS (SELECT * FROM Customers
+WHERE Email = 'smac@kinetecoinc.com')
+
+INSERT INTO Reservations(CustomerID, Date, PartySize)
+SELECT CustomerID, '2022-08-12 18:00:00' AS Date, 5 AS PartySize
+FROM Customers
+WHERE Email = 'smac@kinetecoinc.com';
+
+SELECT * FROM Reservations
+WHERE Date = '2022-08-12 18:00:00';
+
+SELECT * FROM Customers
+ORDER BY CustomerID DESC;
