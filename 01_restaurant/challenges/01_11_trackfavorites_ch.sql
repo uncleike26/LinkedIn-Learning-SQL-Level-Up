97fd4323b702f1796021192c4ca2eceb58e25a28
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-14 20:40:09 +0000

    01-11 Completed

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,14 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad')
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater'; 
+
+SELECT c.FirstName, c.LastName, c.FavoriteDish, d.Name
+FROM Customers AS c 
+INNER JOIN Dishes AS d
+ON c.FavoriteDish = d.DishID
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
