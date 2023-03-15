f97c277bba2845cd02f7736b8c0503fbf3d7d314
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-15 11:03:06 +0000

    02-08 Completed

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -3,5 +3,18 @@
 -- Report 1: Show how many titles were published 
 -- in each year.
 
+SELECT Published, COUNT (DISTINCT Title) AS TotalTitles
+FROM Books
+GROUP BY Published
+ORDER BY COUNT(DISTINCT Title) DESC;
+
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT b.Title, COUNT(LoanID) AS TotalCheckouts
+FROM Books AS b
+INNER JOIN Loans AS l
+ON b.BookID = l.BookID
+GROUP BY Title
+ORDER BY COUNT(LoanID) DESC
+LIMIT 5;
