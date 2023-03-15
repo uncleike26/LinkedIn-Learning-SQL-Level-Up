dcdef292832d5809307c634f83217768d781fb39
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-15 10:54:56 +0000

    02-07 Completed

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,11 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT Title, Barcode 
+FROM Books
+WHERE BookID NOT IN
+(SELECT BookID FROM Loans
+WHERE ReturnedDate IS NULL)
+AND Published BETWEEN 1890 AND 1899
+ORDER BY Title; 
