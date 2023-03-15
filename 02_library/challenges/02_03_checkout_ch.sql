66a6056413b3a9de8fc9a1975629b4530109ea90
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-15 10:08:45 +0000

    02-03 Completed

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,19 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+INSERT INTO Loans(BookID, PatronID, LoanDate, DueDate)
+SELECT b.BookID, p.PatronID, '2022-08-25' AS LoanDate, '2022-09-08' AS DueDate
+FROM (SELECT BookID FROM Books 
+WHERE Barcode IN (2855934983,4043822646)) AS b,
+(SELECT PatronID FROM Patrons
+WHERE Email = 'jvaan@wisdompets.com') AS p;
+
+SELECT l.*, p.FirstName, p.LastName, b.Title
+FROM Loans AS l 
+INNER JOIN Patrons AS p 
+ON p.PatronID = l.PatronID
+INNER JOIN Books AS b
+ON l.BookID = b.BookID
+ORDER BY LoanID DESC 
+LIMIT 2; 
