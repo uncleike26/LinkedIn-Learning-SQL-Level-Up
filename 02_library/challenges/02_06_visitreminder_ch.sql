71c38baf2ad12eb12ffb3c419763e086796f11e5
uncleike26 <127870844+uncleike26@users.noreply.github.com>
2023-03-15 10:49:49 +0000

    02-06 Completed

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,10 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT p.FirstName, p.Email, COUNT(l.LoanID) AS NumLoans
+FROM Patrons AS p
+INNER JOIN Loans AS l
+ON p.PatronID = l.PatronID 
+GROUP BY p.PatronID
+ORDER BY COUNT(l.LoanID) ASC
+LIMIT 9;
